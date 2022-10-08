868478b8fc88f34faa486410b012939bbabc8378
A.A Ryanta <message.driyant@outlook.com>
2022-10-08 15:47:17 +0700

    Small fix styling homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,37 +2,44 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer style={{ background: '#83c81f', color: '#fff', fontFamily: 'Lato', padding: ' 40px 0' }}>
+    <footer
+      style={{
+        background: "#3d3d3d",
+        color: "#fff",
+        fontFamily: "Lato",
+        padding: " 40px 0",
+      }}
+    >
       <div className="container">
         <div className="custom-row-1">
           <div
-            style={{ width: 'calc(100%/3 - 20px)', margin: '0 10px' }}
-          // style={{
-          //   height: "30rem",
-          //   width: 'calc(100%/3 - 20px)',
-          //   backgroundColor: "#e8e8e8",
-          //   paddingTop: "5rem",
-          //   paddingLeft: "1rem",
-          // }}
+            style={{ width: "calc(100%/3 - 20px)", margin: "0 10px" }}
+            // style={{
+            //   height: "30rem",
+            //   width: 'calc(100%/3 - 20px)',
+            //   backgroundColor: "#e8e8e8",
+            //   paddingTop: "5rem",
+            //   paddingLeft: "1rem",
+            // }}
           >
             <h5>Agro Shop</h5>
             <p>
               We are company, Lorem ipsum dolor sit amet consectetur adipisicing
               elit. Voluptates a doloremque quam, corporis illo quisquam eum.
             </p>
+            <p>Jl. Cilandak 132, Jakarta Selatan</p>
             <p>+62 21 927-283-823</p>
-            <p>+62 21 927-283-823</p>
-            <p>+62 21 927-283-823</p>
+            <p>agro@example.com</p>
           </div>
           <div
-            style={{ width: 'calc(100%/3 - 20px)', margin: '0 10px' }}
-          // style={{
-          //   height: "30rem",
-          //   width: 'calc(100%/3 - 20px)',
-          //   backgroundColor: "#e8e8e8",
-          //   paddingTop: "5rem",
-          //   paddingLeft: "1rem",
-          // }}
+            style={{ width: "calc(100%/3 - 20px)", margin: "0 10px" }}
+            // style={{
+            //   height: "30rem",
+            //   width: 'calc(100%/3 - 20px)',
+            //   backgroundColor: "#e8e8e8",
+            //   paddingTop: "5rem",
+            //   paddingLeft: "1rem",
+            // }}
           >
             <h5>Contact us</h5>
             <form action="" className="mt-3">
@@ -55,18 +62,18 @@ const Footer = () => {
                   ></textarea>
                 </div>
               </div>
-              <button className="btn btn-primary btn-sm">Submit</button>
+              <button className="btn btn-sm btn-primary">Submit</button>
             </form>
           </div>
           <div
-            style={{ width: 'calc(100%/3 - 20px)', margin: '0 10px' }}
-          // style={{
-          //   height: "30rem",
-          //   width: 'calc(100%/3 - 20px)',
-          //   backgroundColor: "#e8e8e8",
-          //   paddingTop: "5rem",
-          //   paddingLeft: "1rem",
-          // }}
+            style={{ width: "calc(100%/3 - 20px)", margin: "0 10px" }}
+            // style={{
+            //   height: "30rem",
+            //   width: 'calc(100%/3 - 20px)',
+            //   backgroundColor: "#e8e8e8",
+            //   paddingTop: "5rem",
+            //   paddingLeft: "1rem",
+            // }}
           >
             <h5>Newsletter</h5>
             <p className="mt-3">
@@ -85,7 +92,6 @@ const Footer = () => {
               <button className="btn btn-primary btn-sm">Submit</button>
             </form>
           </div>
-
         </div>
         <p className="text-center mt-4">Copyright 2022 - Agro Shop</p>
       </div>
