551217bb5499b5d20d50c7aeebaf4c52983931be
Mickyokta <micky.okta36@gmail.com>
2022-10-12 17:43:29 +0700

    auction page

--- src/pages/Auction.js
+++ src/pages/Auction.js
@@ -11,7 +11,7 @@ const Auction = () => {
   const { id: AuctionProductId } = useParams();
   const [bidPrice, setBidPrice] = useState(0);
   const [openPrice, setOpenPrice] = useState(11500);
-  const [product, setProduct] = useState({}); //di tempel di page buat tampilan
+  const [product, setProduct] = useState(null); //di tempel di page buat tampilan
   const [roomId, setRoomId] = useState("");
   const [bidHistory, setBidHistory] = useState([]);
   const [currentUserId, setCurrentUserId] = useState(null)
@@ -24,13 +24,21 @@ const Auction = () => {
     } else {
       console.log('bid berhasil')
       setOpenPrice(bidPrice);
+      setBidPrice(bidPrice);
       socket.emit("send-bid", roomId, currentUserId, bidPrice)
     }
   };
   useEffect(() => {
     let user = JSON.parse(localStorage.getItem("user"))
     setCurrentUserId(user.id)
-    // axios.get(`${serverUrl}/auctions/${AuctionProductId}`)
+    axios.get(`${serverUrl}/auctions/${AuctionProductId}`, {
+      headers: {
+        access_token: localStorage.getItem("access_token")
+      }
+    })
+      .then(({ data: productData }) => {
+        setProduct(productData)
+      })
     axios.get(`${mongoServerUrl}/room/${AuctionProductId}`)
       .then(({ data }) => {
         console.log(data)
@@ -40,9 +48,22 @@ const Auction = () => {
     socket.on("send-bid", (payload) => {
       console.log(payload, "socket on send bid")
       setBidPrice(payload.price)
-      // nge append message
+      axios.patch(`${serverUrl}/auctions/${AuctionProductId}/bid`, {
+        lastBidPrice: payload.price
+      }, { headers: { access_token: localStorage.getItem("access_token") } })
     })
   }, [])
+  useEffect(() => {
+    if (product) {
+      if (!product.lastBidPrice) {
+        setBidPrice(product.initPrice)
+        setOpenPrice(product.initPrice)
+      } else {
+        setBidPrice(product.lastBidPrice)
+        setOpenPrice(product.lastBidPrice)
+      }
+    }
+  }, [product])
   useEffect(() => {
     if (roomId) {
       axios.get(`${mongoServerUrl}/history/${roomId}`)
@@ -51,79 +72,81 @@ const Auction = () => {
         })
     }
   }, [roomId])
-  return (
-    <>
-      <div className="container my-4">
-        <div className="row">
-          <h3>Lelang saat ini</h3>
-          <p>Berakhir pada: 07:30</p>
-        </div>
-        <div className="row my-3">
-          <div className="col-8">
-            <div className="row">
-              <div className="col">
-                <img
-                  className="img-thumbnail"
-                  src="https://cdn.pixabay.com/photo/2015/05/26/17/42/pop-corn-785074__480.jpg"
-                  alt="jagung"
-                />
+  if (product) {
+    return (
+      <>
+        <div className="container my-4">
+          <div className="row">
+            <h3>Lelang saat ini</h3>
+            <p>Berakhir pada: 07:30</p>
+          </div>
+          <div className="row my-3">
+            <div className="col-8">
+              <div className="row">
+                <div className="col">
+                  <img
+                    className="img-thumbnail"
+                    src={product.imgUrl}
+                    alt={product.name}
+                  />
+                </div>
+                <div className="col">
+                  <h5>
+                    <strong>{product.name}</strong>
+                  </h5>
+                  <p>Seller: {product.User.fName}</p>
+                  <p>
+                    {product.description}
+                  </p>
+                </div>
               </div>
+            </div>
+            <div
+              className="col-4"
+              style={{ fontSize: "0.875rem", fontFamily: "Lato" }}
+            >
               <div className="col">
                 <h5>
-                  <strong>Jagung</strong>
+                  <strong>Harga lelang saat ini: </strong>
                 </h5>
-                <p>Seller: Micky</p>
-                <p>Rp. 10,000/kg</p>
+                {/* {
+                  product.lastBidPrice ? */}
+                <p className="text-reset">{toIDR(openPrice)}</p>
+                {/* : <p className="text-reset">{toIDR(openPrice)}</p>
+                } */}
+              </div>
+              <div className="col">
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Excepturi sit dolores ea at. Necessitatibus perspiciatis, odio
-                  dolor commodi, voluptate quis corrupti autem sed dolorem
-                  placeat asperiores deleniti distinctio et quisquam!
+                  <strong> Masukkan harga bid kamu</strong>
                 </p>
+                <form className="input-group mb-3" onSubmit={bidHandler}>
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder=""
+                    aria-label="Bid lelang"
+                    aria-describedby="bid"
+                    min="0"
+                    value={bidPrice}
+                    onChange={(e) => setBidPrice(e.target.value)}
+                    step={500}
+                  />
+                  <button
+                    type="submit"
+                    className="btn btn-outline-secondary"
+                    id="bid_btn"
+                  >
+                    Bid
+                  </button>
+                </form>
               </div>
             </div>
           </div>
-          <div
-            className="col-4"
-            style={{ fontSize: "0.875rem", fontFamily: "Lato" }}
-          >
-            <div className="col">
-              <h5>
-                <strong>Harga lelang saat ini: </strong>
-              </h5>
-              <p className="text-reset">{toIDR(openPrice)}</p>
-            </div>
-            <div className="col">
-              <p>
-                <strong> Masukkan harga bid kamu</strong>
-              </p>
-              <form class="input-group mb-3" onSubmit={bidHandler}>
-                <input
-                  type="number"
-                  class="form-control"
-                  placeholder=""
-                  aria-label="Bid lelang"
-                  aria-describedby="bid"
-                  min="0"
-                  value={bidPrice}
-                  onChange={(e) => setBidPrice(e.target.value)}
-                  step={500}
-                />
-                <button
-                  type="submit"
-                  className="btn btn-outline-secondary"
-                  id="bid_btn"
-                >
-                  Bid
-                </button>
-              </form>
-            </div>
-          </div>
+          <div className="row"></div>
         </div>
-        <div className="row"></div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default Auction;
