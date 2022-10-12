9e55bfde5f09276d15f2c794b743d9ad9ec2bb23
anshacerbia <l.anshacerbia@gmail.com>
2022-10-12 10:53:58 +0700

    tes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,10 +13,11 @@ export default function HomePage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { products } = useSelector((state) => state.productReducer);
-  useEffect(async () => {
-    const resp = await dispatch(fetchProducts())
-    console.log(resp);
-    setLatestProduct(resp.slice(0, 4))
+  useEffect(() => {
+    dispatch(fetchProducts())
+    // const respJSON = await resp.json()
+    // // console.log(resp.j);
+    // setLatestProduct(respJSON.slice(0, 4))
   }, []);
   return (
     <div className="container">
@@ -141,7 +142,7 @@ export default function HomePage() {
             </span>
           </div>
         </div>
-        <CardProduct products={latestProduct} />
+        {/* <CardProduct products={latestProduct} /> */}
         {/* <CardProduct /> */}
       </div>
       {/* <div className="custom-row-1">
